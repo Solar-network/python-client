94ed52c5e1117d117ff09a2af92ff18c7cd25a9e
console <60887596+c0nsol3@users.noreply.github.com>
2022-06-08 23:56:51 +0200

    style: apply lint / fmt (#4)

--- client/api/peers.py
+++ client/api/peers.py
@@ -2,20 +2,20 @@ from client.resource import Resource
 
 
 class Peers(Resource):
-
-    def all(self, os=None, status=None, port=None, version=None, order_by=None,
-            page=None, limit=100):
+    def all(
+        self, os=None, status=None, port=None, version=None, order_by=None, page=None, limit=100
+    ):
 
         params = {
-            'os': os,
-            'status': status,
-            'port': port,
-            'version': version,
-            'orderBy': order_by,
-            'page': page,
-            'limit': limit,
+            "os": os,
+            "status": status,
+            "port": port,
+            "version": version,
+            "orderBy": order_by,
+            "page": page,
+            "limit": limit,
         }
-        return self.request_get('peers', params)
+        return self.request_get("peers", params)
 
     def get(self, ip):
-        return self.request_get('peers/{}'.format(ip))
+        return self.request_get("peers/{}".format(ip))
--- client/api/votes.py
+++ client/api/votes.py
@@ -2,13 +2,12 @@ from client.resource import Resource
 
 
 class Votes(Resource):
-
     def all(self, page=None, limit=100):
         params = {
-            'page': page,
-            'limit': limit,
+            "page": page,
+            "limit": limit,
         }
-        return self.request_get('votes', params)
+        return self.request_get("votes", params)
 
     def get(self, vote_id):
-        return self.request_get('votes/{}'.format(vote_id))
+        return self.request_get("votes/{}".format(vote_id))
