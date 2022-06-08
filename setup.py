a1f76851ac902794fb566f30000bcaad6b33b2cf
console <60887596+c0nsol3@users.noreply.github.com>
2022-06-08 23:37:11 +0200

    build: improve setup and requirements (#3)

--- setup.py
+++ setup.py
@@ -3,21 +3,26 @@ import sys
 import setuptools
 
 
-requires = [
-    'requests>=2.19.1',
-    'backoff>=1.6.0',
-    'flatten_dict>=0.3.0'
-]
+# Utility function to read the README file.
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+requires = ['requests', 'backoff', 'flatten_dict']
 
 tests_require = [
-    'flake8>=3.5.0',
-    'flake8-import-order>=0.17.1',
-    'flake8-print>=3.1.0',
-    'flake8-quotes>=1.0.0',
-    'pytest>=3.6.1',
-    'pytest-responses>=0.3.0',
-    'pytest-mock>=1.10.0',
-    'pytest-cov>=2.5.1'
+    "flake8==4.0.1",
+    "flake8-import-order==0.18.1",
+    "flake8-print==4.0.1",
+    "flake8-quotes==3.3.1",
+    "pytest==7.0.1; python_version < '3.7'",
+    "pytest==7.1.2; python_version >= '3.7'",
+    "isort==5.10.1",
+    "pytest-cov==2.5.1",
+    "pytest-responses>=0.3.0",
+    "pytest-mock>=1.10.0",
+    "black==22.3.0",
+    "build==0.8.0",
+    "twine==3.8.0",
 ]
 
 extras_require = {
@@ -35,8 +40,11 @@ setuptools.setup(
     author_email='hello@solar.org',
     url='https://github.com/solar-network/python-client',
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=requires,
     extras_require=extras_require,
     tests_require=tests_require,
     setup_requires=setup_requires,
+    python_requires=">=3.6",
 )
